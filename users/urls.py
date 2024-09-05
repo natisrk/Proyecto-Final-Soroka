71068ddd7cc4b541431f7e947bdb5869bd3a4373
natalia soroka <nati.soroka@gmail.com>
2024-09-05 11:32:15 -0300

    correccion

--- users/urls.py
+++ users/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     path('register/', views.register, name="Register"),
     path('logout/', LogoutView.as_view(template_name='plantillas/users/sesioncerrada.html'), name="Logout"),
     path('edit/', views.editar_perfil, name="EditarPerfil"),
-    path('ambiar_pass/', views.CambiarContrasenia.as_view(), name="Cambiar_pass"),
+    path('Cambiar_pass/', views.CambiarContrasenia.as_view(), name="Cambiar_pass"),
     
 ]
     
